b0e2e297713f2c44073caf142f34f69913227f40
Aleksandr-Pod <stdfire@gmail.com>
2022-02-04 23:42:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@ import onClick from "./onClick.js";
 
 const ref = document.querySelector('h1');
 ref.addEventListener('click', (evt) => onClick(evt, ref));
-
+// вызов анонимной ф-ции - если назвать, то как передать дальше?
