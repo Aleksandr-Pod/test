41fdede6ca7f3ebc72aba0829a0712c798665987
Aleksandr-Pod <stdfire@gmail.com>
2022-02-04 23:26:31 +0200

    Update onClick.js

--- onClick.js
+++ onClick.js
@@ -2,7 +2,7 @@
 export default function onClick(evt, ref) {
     console.log(evt);
     console.log("data:", ref);
-    ref.removeEventListener('click', onClick);
+    ref.removeEventListener('click', onClick); // теряется ссылка на ф-цию
     console.log("listener removed");
     return;
 }
